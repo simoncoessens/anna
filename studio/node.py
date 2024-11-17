5e05e284161a8be0661ee0a70f3eee0b93b6195b
Qasim Khan <qasimkhan3.124@gmail.com>
2024-11-17 15:23:28 +0100

    add explanations

--- studio/node.py
+++ studio/node.py
@@ -8,6 +8,45 @@ from pydantic import BaseModel, Field
 from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
+from pinecone import Pinecone
+import os
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+index = pc.Index("aiact")
+
+def fetch_context(article_ids):
+    all_fetched_texts = []
+
+    for article_id in article_ids:
+        # Try to fetch the article normally
+        result = index.fetch(ids=[article_id])
+
+        if article_id in result["vectors"]:
+            # Article found
+            all_fetched_texts.append(
+                f"{article_id}:\n{result['vectors'][article_id]['metadata']['text']}"
+            )
+        else:
+            # Article not found, try to fetch subsections
+            subsection_ids = [f"{article_id}.{i:03d}" for i in range(1, 100)]
+            result = index.fetch(ids=subsection_ids)
+
+            # Keep track of whether any subsections were found
+            subsections_found = False
+
+            # Process fetched subsections
+            for subsection_id in subsection_ids:
+                if subsection_id in result["vectors"]:
+                    subsections_found = True
+                    all_fetched_texts.append(
+                        f"{subsection_id}:\n{result['vectors'][subsection_id]['metadata']['text']}"
+                    )
+
+            if not subsections_found:
+                # No subsections found, print a message to the terminal
+                print(f"No article found for ID: {article_id}")
+
+    return "\n\n".join(all_fetched_texts)
 
 
 class Q1_SubquestionAnswers(BaseModel):
@@ -35,7 +74,7 @@ class Q1_SubquestionAnswers(BaseModel):
 
     def __iter__(self):
         for attr in ["q1", "q2", "q3", "q4", "q5"]:
-            yield getattr(self, attr) 
+            yield getattr(self, attr)
 
 
 prompt = """
@@ -92,6 +131,101 @@ def check_completion(state: GraphState) -> bool:
     return all_not_none
 
 
+# Define the function to process the user's response
+def process_response(state: GraphState) -> GraphState:
+    messages = state["messages"]
+    # user response is every even number message
+    user_response = ""
+    for i in range(0, len(messages), 2):
+        user_response += messages[i].content + "\n"
+    sub_answers_new = q1_chain.invoke({"response": user_response})
+    if "subquestion_answers" in state:
+        sub_answers_existing = state["subquestion_answers"]
+    else:
+        sub_answers_existing = Q1_SubquestionAnswers()
+    for attr in ["q1", "q2", "q3", "q4", "q5"]:
+        if not getattr(sub_answers_existing, attr):
+            setattr(sub_answers_existing, attr, getattr(sub_answers_new, attr))
+    return {"subquestion_answers": sub_answers_existing}
+
+class subquestion_explanations(BaseModel):
+    q1: str = Field(None, description="Is the system a machine-based system?")
+    q2: str = Field(None, description="May the system exhibit adaptiveness after deployment?")
+    q3: str = Field(None, description="Is it capable of inferring how to generate outputs?")
+    q4: str = Field(None, description="Can the system outputs influence environments?")
+    q5: str = Field(None, description="Is the system designed with varying autonomy levels?")
+
+    def all(self):
+        return all([self.q1, self.q2, self.q3, self.q4, self.q5])
+    
+    def all_not(self):
+        return all([not self.q1, not self.q2, not self.q3, not self.q4, not self.q5])
+    
+    def __iter__(self):
+        for attr in ['q1', 'q2', 'q3', 'q4', 'q5']:
+            yield getattr(self, attr)
+    
+    def combine_answers(self):
+        combined = "\n".join([f"{getattr(self, attr)}" for attr in ['q1', 'q2', 'q3', 'q4', 'q5']])
+        return combined
+
+# Function to analyze the history and generate explanations
+def analyze_history_responses(history, explanation_chain):
+    # Invoke the explanation chain with the formatted history
+    analysis = explanation_chain.invoke({"history": history})
+    return analysis
+
+def generate_agent_explanations(history, explanation_chain):
+    # Generate the analysis
+    analysis = analyze_history_responses(history, explanation_chain)
+    return analysis.combine_answers()
+
+
+# Define the explanation prompt
+explanation_prompt = """
+Given the user's history of responses, analyze whether all the answers to the following questions are "True" If all answers are True provide a deatiled explanation for each response to justify why it was marked as "True"
+
+History of responses:
+{history}
+
+Questions:
+1. Is the system a machine-based system?
+2. May the system exhibit adaptiveness after deployment?
+3. Is it capable, for explicit or implicit objectives, of inferring, from the input it receives, how to generate outputs such as predictions, content, recommendations, or decisions?
+4. Can the system outputs influence physical or virtual environments?
+5. Is the system designed to operate with varying levels of autonomy?
+
+Output:
+- For each question: "[Explanation]"
+
+Be cautious to only provide explanations where there is explicit, unambiguous information in the user's responses.
+""".strip()
+# Define the PromptTemplate
+explanation_prompt_template = PromptTemplate(input_variables=["history"], template=explanation_prompt)
+
+# Assuming `gpt.with_structured_output` is a valid LLM wrapper and `subquestion_explanations` is defined:
+llm_explanation = gpt.with_structured_output(subquestion_explanations)
+explanation_chain = explanation_prompt_template | llm_explanation
+
+# Define the function to process the user's response
+def generate_explanations(state: GraphState) -> GraphState:
+    messages = state["messages"]
+    # user response is every even number message
+    user_response = ""
+    for i in range(0, len(messages), 2):
+        user_response += messages[i].content + "\n"
+    return {
+        "explanations": generate_agent_explanations(user_response, explanation_chain)
+    }
+
+
+# Define the function to check if all subquestions are answered
+def check_completion(state: GraphState) -> bool:
+    answers = state["subquestion_answers"]
+    all_not_none = all([answer is not None for answer in answers])
+    return all_not_none
+
+
 # Define the function to inform the user about vague parts
 def inform_user(state: GraphState) -> str:
     vague_parts = [
@@ -114,7 +248,38 @@ def inform_user(state: GraphState) -> str:
         f"{vague_str}\n\n"
         "Please provide more details to clarify these aspects."
     )
-    return {"messages": AIMessage(content=suggestion)}
+    question = state["messages"][-1].content
+    general_template = """
+You are Anna, the AI assistant here to guide users in answering the **Main Question**: {question}. Your role is to provide clear, supportive assistance while keeping things conversational and concise.
+
+**Summary of Missing Details**: The following parts of your description are unclear:
+{suggestion}
+
+Please encourage the user to clarify these aspects so they can progress smoothly with the questionnaire. 
+
+If the user has asked a general question, respond directly with relevant information from the **AI Act Articles** below. Keep your response short, friendly, and focus on the user's question or the next steps they should take to complete the questionnaire accurately.
+If you reference AI articles, then reference them and put the article name in bold.
+
+**Relevant AI Act Articles**: 
+{articles}
+""".strip()
+
+    general_prompt = PromptTemplate(
+        template=general_template,
+        partial_variables={
+            "articles": fetch_context(
+                article_ids=[
+                    "article_003",
+                    "recital_rct_12",
+                    "recital_rct_97",
+                    "recital_rct_100",
+                ]
+            )
+        },
+    )
+    chain = general_prompt | gpt
+    response = chain.invoke({"question": question, "suggestion": suggestion})
+    return {"messages": response}
 
 
 # Initialize the StateGraph
@@ -123,11 +288,13 @@ graph_builder = StateGraph(GraphState)
 # Add nodes to the graph
 graph_builder.add_node("process_response", process_response)
 graph_builder.add_node("inform_user", inform_user)
+graph_builder.add_node("generate_explanations", generate_explanations)
 
 # Define the edges with conditions
 graph_builder.add_edge(START, "process_response")
-graph_builder.add_conditional_edges(
-    "process_response", check_completion, {True: END, False: "inform_user"}
-)
+graph_builder.add_conditional_edges("process_response", check_completion, {True: END, False: "inform_user"})
 graph_builder.add_edge("inform_user", END)
-graph = graph_builder.compile()
+graph_builder.add_edge("generate_explanations", END)
+# Compile the graph
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
